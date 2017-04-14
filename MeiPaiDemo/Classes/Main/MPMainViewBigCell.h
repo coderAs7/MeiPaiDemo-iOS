44b7055c9f3ff463a2ec729ca6cb0282e2c9e883
李明明 <ming-ming.li@utsoft.cn>
2017-04-14 14:04:32 +0800

    增加首页布局UI
    增加首页布局UI

---
+++ MeiPaiDemo/Classes/Main/MPMainViewBigCell.h
@@ -0,0 +1,13 @@
+//
+//  MPMainViewBigCell.h
+//  MeiPaiDemo
+//
+//  Created by 李明 on 2017/4/14.
+//  Copyright © 2017年 UTOUU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPMainViewBigCell : UICollectionViewCell
+@property (nonatomic, strong) id data;
+@end
