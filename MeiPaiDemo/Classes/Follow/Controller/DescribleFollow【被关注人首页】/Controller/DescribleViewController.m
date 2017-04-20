dc52e857f7f0b93533d0ab01af61a479baad64bb
刘虎 <hu.liu@utsoft.cn>
2017-04-20 17:07:57 +0800

    1.编写被关注人UI界面 2.被关注人UI界面视频播放以及逻辑判断

--- MeiPaiDemo/Classes/Follow/Controller/DescribleFollow【被关注人首页】/Controller/DescribleViewController.m
+++ MeiPaiDemo/Classes/Follow/Controller/DescribleFollow【被关注人首页】/Controller/DescribleViewController.m
@@ -8,7 +8,9 @@
 
 #import "DescribleViewController.h"
 #import "DescribleView.h"
-@interface DescribleViewController ()
+@interface DescribleViewController () {
+    DescribleView *view;
+}
 
 @end
 
@@ -30,23 +32,14 @@
     [super viewDidLoad];
     self.view.backgroundColor = RGB(45, 47, 55);
     
-    DescribleView *view = [[DescribleView alloc]initWithFrame:self.view.frame];
+    view = [[DescribleView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT - 44)];
     [self.view addSubview:view];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)dealloc {
+    [view removeFromSuperview];
+    view = nil;
 }
-*/
 
 @end
